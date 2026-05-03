69aecefaa986b2c1e7a00daf24fcef49b417bc48
ayushacharya <igayushacharya@gmail.com>
2026-05-03 09:14:40 +0530

    update in code

--- src/main/java/com/jpmc/midascore/listener/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/listener/TransactionListener.java
@@ -1,16 +1,18 @@
 package com.jpmc.midascore.listener;
 
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import com.jpmc.midascore.foundation.Transaction;
 
-import org.springframework.kafka.annotation.KafkaListener;
-
 @Component
 public class TransactionListener {
-	
-	@KafkaListener(topics = "${general.kafka-topic}")
-	public void listener(Transaction transaction) {
-		System.out.println("Received: " + transaction.getAmount());
-	}
+
+    @KafkaListener(
+        topics = "${general.kafka-topic}",
+        groupId = "midas-group"
+    )
+    public void listen(Transaction transaction) {
+        System.out.println("Received: " + transaction.getAmount());
+    }
 }
